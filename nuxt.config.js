3006699c6b96eaf781195ed7dfcaa999f583ccf6
bryandonmarc <bryan.donmarc@gmail.com>
2020-12-23 15:04:27 +0800

    chore: try fixing nuxt build

--- nuxt.config.js
+++ nuxt.config.js
@@ -117,4 +117,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-builddir
+  buildDir: 'public',
 }
